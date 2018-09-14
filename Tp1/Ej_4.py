5c8a09f0fb017568041159a436a33f0648d0bf52
matias.leizerovich <matias.leize@gmail.com>
2018-09-14 17:36:51 -0300

    Version de Mati del ej 4

--- Tp1/Ej_4.py
+++ Tp1/Ej_4.py
@@ -6,21 +6,37 @@ from lectura import ldata
 from histograma import histograma
 
 
-net_science = read_gml('/home/tomas/Desktop/Redes complejas/Urdimbres-Sofisticadas/Tp1/tc01_data/netscience.gml')
-july = read_gml('/home/tomas/Desktop/Redes complejas/Urdimbres-Sofisticadas/Tp1/tc01_data/as-22july06.gml')
+net_science = read_gml('/home/matias/Documentos/Facultad/Redes/Urdimbres-Sofisticadas/Tp1/tc01_data/netscience.gml')
+july = read_gml('/home/matias/Documentos/Facultad/Redes/Urdimbres-Sofisticadas/Tp1/tc01_data/as-22july06.gml')
 
  
-lista = nx.average_neighbor_degree(july)
+lista_1 = nx.average_neighbor_degree(july)
+lista_2 = nx.average_neighbor_degree(net_science)
 
-vecinos_degree = lista.values()
-nodes = lista.keys()
-    
-
-k, values = np.histogram(vecinos_degree)
-
-plt.figure(1)
-plt.plot(k, '.')
-plt.yscale('log')
-plt.xscale('log')
-
-plt.hist(vecinos_degree,bins=int(len(vecinos_degree)**0.5))
+vecinos_degree_1 = lista_1.values()
+nodes_1 = lista_1.keys()
+#    
+vecinos_degree_2 = lista_2.values()
+nodes_2 = lista_2.keys()
+#%%
+#k_1, values_1 = np.histogram((list(vecinos_degree_1)))
+#k_2, values_2 = np.histogram(list(vecinos_degree_2))
+#
+#cumulative = np.cumsum(k)
+## plot the cumulative function
+#plt.figure()
+#plt.plot(values_1[:-1], cumulative, '.b')
+#plt.figure()
+#plt.plot(values_2[:-1], cumulative, '.r')
+#
+#
+##%%
+#plt.figure()
+#plt.plot(k, '.')
+#plt.yscale('log')
+#plt.xscale('log')
+#
+#plt.hist(vecinos_degree_1,bins=int(len(vecinos_degree_1)**0.5))
+#
+##%%
+#sor
