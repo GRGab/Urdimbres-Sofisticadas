af65688d3eeb5f9a572423aebdf0c968936b55e3
matias.leizerovich <matias.leize@gmail.com>
2018-10-01 18:10:14 -0300

    Emprolije la carpeta Tp1.

--- Tp1/dolphins_funciones.py
+++ Tp1/dolphins_funciones.py
@@ -68,10 +68,53 @@ def contar_enlaces_entre_grupos(g, atributo):
             n = n + 1
     return n
 
-#Calculo del p-value
+################## Calculo del p-value
 def p_value(datos, valor_real=52):
     enlace = list(Counter(datos).keys())
     cuantos = list(Counter(datos).values())
     indices = np.where(np.array(enlace)<= valor_real)[0] 
     a = [cuantos[i] for i in indices]
     return 2 * len(a)/len(datos)
+
+
+################## Para desarmar la red
+    
+#Esta funcion agarra un grafo, y lo va desarmando sacando una n cantidad de nodos
+#con el criterio de que esos nodos sean los de coeficiente de clustering mas chico
+#de la red.     La funcion tiene input el grafo (g), y el porcentaje de nodos que 
+#queres sacarle a la red (porcentaje_nodos_sacados). El porcentaje se escribe
+#como 0.1 para sacar el 10% de los nodos.
+
+def desarme(g, porcentaje_nodos_sacados):
+    n= int(len(g.nodes.keys()) * porcentaje_nodos_sacados)
+    G = g.copy()
+    nodes = []
+    clustering = []
+    for a in G.nodes.keys():
+        if G.degree(a)>1:
+            nodes.append(a)
+            clustering.append(nx.clustering(G)[a])
+    for i in range(n):
+        j = clustering.index(min(clustering))
+        G.remove_node(nodes[j])
+        clustering.pop(j)
+        nodes.pop(j)
+    return G
+
+def cociente(g):
+    '''Agarra una red G y va sacando una cierta proporcion de nodos hasta que
+    el cociente entre la cant de nodos de la componente gigante y la segunda 
+    comoponente sea cercano a 1.'''
+    rango_porcentajes = np.arange(0, 0.85, 1/g.order())
+    d = []
+    for i in rango_porcentajes:
+        g_desarmado = desarme(g, i)
+        lengths = [len(d) for d in sorted(nx.connected_components(g_desarmado), key=len, reverse=True)]
+        if len(lengths)>=2:
+            d.append(lengths[0]/lengths[1])
+    d2 = np.asarray(d)
+    d2 = abs(d2-1)
+    d2 = list(d2)
+    index = d2.index(min(d2))
+    porcentaje = rango_porcentajes[index]
+    return porcentaje, min(d)
