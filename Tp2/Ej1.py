43016bc9d85c2f209e55aee629ab91cc9fdc7006
tomasferreirachase <tomaschase96@gmail.com>
2018-10-03 19:52:25 -0300

    Va el grafico y codigo para hacer la figura 1A de Zotenko.

--- Tp2/Ej1.py
+++ Tp2/Ej1.py
@@ -40,25 +40,50 @@ print(df)
 
 #%% 
 # Le agrego categoria de esencial a los nodos correspondientes.
-dict_lit = dict(g_lit.nodes())
-for nodo in g_lit.nodes():
-    for i in range(len(ess)):
-        for j in range(len(ess[i])):
-            if str(nodo) == ess[i][j]:
-                dict_lit[nodo] = 1
-            else:
-                dict_lit[nodo] = 0
+
+def agregar_importancia(G):
+    g = list(G.nodes())
+    value = np.zeros([len(g)])
+    for h in range(len(g)):
+        for i in range(len(ess)):
+            for j in range(len(ess[i])):
+                if g[h] == ess[i][j]:
+                    value[h] = 1
+                    break
+    return g, value
 
 
 
+def desarme(g):
+    nodos, values = agregar_importancia(g)
+    nodos = []
+    k = []
+    for a, b in g.degree():
+        nodos.append(a)
+        k.append(b)
+    k_norm = [i/max(k) for i in k]
+    threshold = np.arange(0.01, 1, 0.001)
+    y = []
+    x = []
+    for j in threshold:
+        y_i = 0
+        x_i = 0
+        for i in range(len(nodos)):
+            if k_norm[i]>j:
+                if values[i] == 1:
+                    y_i += 1
+                x_i += 1
+        if x_i != 0:
+            y.append(y_i/x_i)
+            x.append(x_i/len(nodos))
+    return y, x
 
+y_0, x_0 = desarme(g_ap)
+y_1, x_1 = desarme(g_lit)
+y_2, x_2 = desarme(g_y2h)
 
-
-
-
-
-
-
-
-
+plt.plot(x_0, y_0, label = 'Ap')
+plt.plot(x_1, y_1, label = 'Lit')
+plt.plot(x_2, y_2, label = 'Y2H')
+plt.legend()
 
