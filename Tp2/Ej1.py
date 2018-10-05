d02abd170b73a8e8101d19f9f925c4982aebfd8d
tomasferreirachase <tomaschase96@gmail.com>
2018-10-05 17:21:26 -0300

    Va la primera parte del D en el archivo Ej1.py

--- Tp2/Ej1.py
+++ Tp2/Ej1.py
@@ -6,6 +6,7 @@ from lectura import ldata
 import sys
 sys.path.append('./Tp2/')
 import pandas as pd
+from scipy.odr import Model, RealData, ODR
 
 ess = ldata('tc02Data/Essential_ORFs_paperHe.txt')
 ap = ldata('tc02Data/yeast_AP-MS.txt')
@@ -54,7 +55,7 @@ def agregar_importancia(G):
 
 
 
-def desarme(g):
+def ess_vs_k(g):
     nodos, values = agregar_importancia(g)
     nodos = []
     k = []
@@ -78,12 +79,92 @@ def desarme(g):
             x.append(x_i/len(nodos))
     return y, x
 
-y_0, x_0 = desarme(g_ap)
-y_1, x_1 = desarme(g_lit)
-y_2, x_2 = desarme(g_y2h)
+y_0, x_0 = ess_vs_k(g_ap)
+y_1, x_1 = ess_vs_k(g_lit)
+y_2, x_2 = ess_vs_k(g_y2h)
 
 plt.plot(x_0, y_0, label = 'Ap')
 plt.plot(x_1, y_1, label = 'Lit')
 plt.plot(x_2, y_2, label = 'Y2H')
 plt.legend()
 
+#%%
+# Ej D-1. Recontra chequear.
+
+def Linear(M, x):
+    """
+    Funcion lineal para ajustar con el ODR:
+    
+    >>> from scipy.odr import Model, RealData, ODR
+    >>> linear_model = Model(Linear)
+    >>> data = RealData(X, Y, sx=X_err, sy=Y_err)
+    >>> odr = ODR(data, linear_model, beta0=[0., 1.])
+    >>> out = odr.run()
+    
+    >>> m = out.beta[0]
+    >>> b = out.beta[1]
+    >>> m_err = out.sd_beta[0]
+    >>> b_err = out.sd_beta[1]        
+    >>> chi2 = out.res_var
+    .
+    .
+    """
+    m, b = M
+    return m*x + b
+
+def desarme(g):
+    nodos, values = agregar_importancia(g)
+    nodos = []
+    k = []
+    for a, b in g.degree():
+        nodos.append(a)
+        k.append(b)
+    threshold = np.arange(0, max(k), 1)
+    y = []
+    x = []
+    for j in threshold:
+        y_i = 0
+        x_i = 0
+        for i in range(len(nodos)):
+            if k[i] == j:
+                x_i += 1
+                if values[i] == 1:
+                    y_i += 1
+        if x_i != 0:
+            y.append(y_i/x_i)
+            x.append(j)
+    return x, y
+
+
+k, y_0 = desarme(g_lit)
+y_1 = [np.log(1-i) for i in y_0]
+plt.plot(k[:10], y_1[:10], 'o', label = 'Lit')
+plt.legend()
+linear_model = Model(Linear)
+data = RealData(k[:10], y_1[:10])
+odr = ODR(data, linear_model, beta0=[0., 1.])
+out = odr.run()
+plt.plot(k[:10], [out.beta[0]*k[i]+out.beta[1] for i in range(1,11)], 'b-')
+
+k, y_0 = desarme(g_ap)
+y_1 = [np.log(1-i) for i in y_0]
+plt.plot(k[:10], y_1[:10], 'g^', label = 'Ap')
+plt.legend()
+linear_model = Model(Linear)
+data = RealData(k[:10], y_1[:10])
+odr = ODR(data, linear_model, beta0=[0., 1.])
+out = odr.run()
+plt.plot(k[:10], [out.beta[0]*k[i]+out.beta[1] for i in range(1,11)], 'g-')
+
+k, y_0 = desarme(g_y2h)
+y_1 = [np.log(1-i) for i in y_0]
+plt.plot(k[:10], y_1[:10], 'r*', label = 'Y2H')
+plt.legend()
+linear_model = Model(Linear)
+data = RealData(k[:10], y_1[:10])
+odr = ODR(data, linear_model, beta0=[0., 1.])
+out = odr.run()
+plt.plot(k[:10], [out.beta[0]*k[i]+out.beta[1] for i in range(1,11)], 'r-')
+
+plt.ylabel('log(1-P)')
+plt.xlabel('Protein connectivity (k)')
